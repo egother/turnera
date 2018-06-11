c949fff20a5ea9afef12aeb3d7fd0b74b5dfda71
Eze <egomez.ogg@gmail.com>
2018-06-11 18:59:54 -0300

    cambios

---
+++ enviarSolicitud.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+    <?php
+        $nombre = $_POST["nombre"];
+        $mail = $_POST["email"];
+        $esp = $_POST["especialidad"];
+        $msj = $_POST["mensaje"];
+    
+        echo $nombre,$mail,$esp,$msj;
+    ?>
+</body>
+</html>
+
